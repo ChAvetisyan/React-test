6d87cc5924d5bdf57c3ed9988e5869fee5551ab1
ChAvetisyan <ch.avetisyan@gmail.com>
2023-01-06 21:35:11 +0400

    05-01

---
+++ react-app/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+class Header extends Component {
+    render(){
+        return <>
+            <p>text</p>
+        </>
+    }
+}
+
+export default Header
